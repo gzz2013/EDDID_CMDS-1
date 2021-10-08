e9f93399e5d77871f68ceb8a4c9fb78b83c4a883
ganjiexiang <544162008@qq.com>
2021-10-08 13:27:16 +0800

    2021年10月8日 13点27分

--- run.py
+++ run.py
@@ -17,6 +17,6 @@ if __name__ == '__main__':
     HTMLReport.TestRunner(
         title="EDDID_CDMS项目接口测试",
         description="如有疑问请联系-ganjiexiang",
-        report_file_name="EDDID_CDMS项目接口测试11",
+        report_file_name="EDDID_CDMS项目接口测试",
         thread_count=1
     ).run(suite)
