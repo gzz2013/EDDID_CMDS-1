2067456bb8916ca1945e0bb02e3e9c4a4a3c1e68
ganjiexiang <544162008@qq.com>
2021-11-26 09:42:30 +0800

    2021年11月26日 09点42分

--- run.py
+++ run.py
@@ -6,16 +6,16 @@ from Test_Suite import suite_用户创建及审批, suite_用户创建结构性�
 
 suite = unittest.TestSuite()
 
-# suite.addTests(suite_用户创建及审批.get_suite_creatUser新建用户())
-# suite.addTests(suite_换汇申请单.get_suite_CreateExchange新建换汇申请单())
+suite.addTests(suite_用户创建及审批.get_suite_creatUser新建用户())
+suite.addTests(suite_换汇申请单.get_suite_CreateExchange新建换汇申请单())
 suite.addTests(suite_用户创建结构性产品及审批.get_suite_creatUser没有结构性产品账号及申请结构性产品审批())
-# suite.addTests(suite_出金申请单.get_suite_CreatEquitiesWithdrawal出金())
-# suite.addTests(suite_入金申请单.get_suite_CreatEquitiesDeposit入金())
-# suite.addTests(suite_大额入金申请单.get_suite_CreatEquitiesDeposit大额入金())
-# suite.addTests(suite_用户创建全类型交易账户及审批.get_suite_creatUser新建所有类型用户())
-# suite.addTests(suite_用户创建及审批后停用.get_suite_creatUser新建用户后停用())
-# suite.addTests(suite_停用账号操作开启.get_suite_enableAcct停用后开启())
-# suite.addTests(suite_账号关闭.get_suite_closeAcct账号关闭())
+suite.addTests(suite_出金申请单.get_suite_CreatEquitiesWithdrawal出金())
+suite.addTests(suite_入金申请单.get_suite_CreatEquitiesDeposit入金())
+suite.addTests(suite_大额入金申请单.get_suite_CreatEquitiesDeposit大额入金())
+suite.addTests(suite_用户创建全类型交易账户及审批.get_suite_creatUser新建所有类型用户())
+suite.addTests(suite_用户创建及审批后停用.get_suite_creatUser新建用户后停用())
+suite.addTests(suite_停用账号操作开启.get_suite_enableAcct停用后开启())
+suite.addTests(suite_账号关闭.get_suite_closeAcct账号关闭())
 
 if __name__ == '__main__':
     HTMLReport.TestRunner(
