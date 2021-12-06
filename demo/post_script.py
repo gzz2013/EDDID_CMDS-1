c5178aa2cf8fcc65fe895c3c9840d5d1ed8b8357
ganjiexiang <544162008@qq.com>
2021-12-06 18:53:44 +0800

    18点53分

--- demo/post_script.py
+++ demo/post_script.py
@@ -1,9 +1,17 @@
 import requests
 import time
+import smtplib
+from email.mime.text import MIMEText
 
 #tokenid为机器人的token，text为关键字
+
+
+
+localtime = time.strftime("%Y-%m-%d %H:%M", time.localtime())
 def send2robot(tokenid, text):
     s = requests.Session()
+    global localtime
+    localtime= time.strftime("%Y-%m-%d %H:%M", time.localtime())
     headers = {
         "Content-Type": "application/json",
     }
@@ -18,7 +26,7 @@ def send2robot(tokenid, text):
     # }
     #获取当前时间
     #有超链接的data
-    localtime=time.strftime("%Y-%m-%d %H:%M", time.localtime())
+
     print("当前时间为：",localtime)
     data ={
         "msgtype": "link",
@@ -35,11 +43,38 @@ def send2robot(tokenid, text):
     print("send2robotre::",send2robotre.text)
     return send2robotre
 
+def post_Email(username):
+
+    mailserver = "smtp.exmail.qq.com"  #邮箱服务器地址
+    username_send = 'zack.gan@edsz9.com'  #邮箱用户名
+    password = 'W9d7Dd48tC76xeMC'   #邮箱密码：需要使用授权码
+    # username_recv = "ganjiexiang@qq.com,544162008@qq.com"#收件人，多个收件人用逗号隔开
+    username_recv = username
+
+    message = '''
+    <p><a href="http://192.168.57.23:8080/job/cdm_automated_testing/HTML_20Report/">自动化测试报告传送门</a></p>
+    <p>-----如有疑问可咨询：甘杰祥</p>
+    '''
+    mail = MIMEText(message, 'html', _charset="utf-8")
+    mail['Subject'] = 'CMDS自动化测试报告'+localtime
+    mail['From'] = username_send  #发件人
+    mail['To'] = username_recv  #收件人；[]里的三个是固定写法，别问为什么，我只是代码的搬运工
+    smtp = smtplib.SMTP_SSL(mailserver,port=465) # 连接邮箱服务器，smtp的端口号是25
+    # smtp=smtplib.SMTP_SSL('smtp.qq.com',port=465) #QQ邮箱的服务器和端口号
+    smtp.login(username_send,password)  #登录邮箱
+    smtp.sendmail(from_addr = username_send, to_addrs=username_recv.split(','), msg=mail.as_string())
+    smtp.quit() # 发送完毕后退出smtp
+    print ('success')
+
+
 
 #tokenid为机器人的token，text为关键字
+# 阿里群
 # send2robot("fb8e9870d1a020b0e0b4c139b9455c399d39c80030545c7ced062afa5b5dac6a","CMDS自动化测试报告")
+# 公司群
 send2robot("38f7002387ae37a28bac18bd3b3fe6289de0703cf66994f65288a2b1fb9d35a1","CMDS自动化测试报告")
-
+#添加收件邮箱用”,“隔开
+post_Email("ganjiexiang@qq.com,jira@edsz9.com,Jenny.zhang@edsz9.com,zack.gan@edsz9.com")
 # if __name__ == "__main__":
 #     # result=send2robot(sys.argv[1], sys.argv[2])
 #     token='fb8e9870d1a020b0e0b4c139b9455c399d39c80030545c7ced062afa5b5dac6a'
